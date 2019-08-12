805d1284adde5eeb02e84972d970b2aeaa31ffda
YellowFlash701 <jeuehdbdue27@gmail.com>
2019-08-12 18:52:41 +0900

    Fix failing unit test

--- scripts/translate_readme.sh
+++ scripts/translate_readme.sh
@@ -3,3 +3,5 @@ pandoc --from=markdown --to=rst --output=README.rst README.md
 
 
 # [ai:2026-07-23] # Add error handling for missing translation tools
+
+# [ai:2026-07-23] Update README for new features #TODO
